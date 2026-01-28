e2cf590bf7e9efa56fab8837d4c836a68434f3b0
Kenn0679 <kenn0679@gmail.com>
2026-01-28 09:32:48 +0700

    feat: add Sheet and Sidebar components for improved UI layout
    - Implemented Sheet component using Radix UI for modal-like functionality.
    - Created Sidebar component with collapsible and mobile-friendly design.
    - Added Skeleton component for loading states.
    - Introduced Tooltip component for enhanced user interactions.
    - Developed useIsMobile hook to manage mobile responsiveness.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import { router } from './routers'
 import { useState } from 'react'
 import { UserContext, type User } from './contexts/UserContext'
 import { ThemeProvider } from './DarkMode/theme-provider'
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar'
 
 function App() {
   const [user, setUser] = useState<User | null>(null)
   return (
     <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-      <UserContext.Provider value={{ user, setUser }}>
-        <RouterProvider router={router} />
-      </UserContext.Provider>
+      <SidebarProvider>
+        <UserContext.Provider value={{ user, setUser }}>
+          <RouterProvider router={router} />
+          <SidebarTrigger />
+        </UserContext.Provider>
+      </SidebarProvider>
     </ThemeProvider>
   )
 }
